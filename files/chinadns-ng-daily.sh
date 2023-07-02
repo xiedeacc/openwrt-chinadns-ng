c658ea419d38443e767113415e2ee4d2c46ab154
tiger <tiger@xiedeacc.com>
2023-07-02 17:23:41 +0800

    udpate daily update

--- files/chinadns-ng-daily.sh
+++ files/chinadns-ng-daily.sh
@@ -2,18 +2,38 @@
 
 [ -d /etc/chinadns-ng ] || mkdir /etc/chinadns-ng
 
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute.txt -O /etc/chinadns-ng/chnroute.tmp
-mv -f /etc/chinadns-ng/chnroute.tmp /etc/chinadns-ng/chnroute.txt
+set -o errexit
+set -o pipefail
 
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute6.txt -O /etc/chinadns-ng/chnroute6.tmp
-mv -f /etc/chinadns-ng/chnroute6.tmp /etc/chinadns-ng/chnroute6.txt
+url='https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
+echo "$data" | awk -F/ '{print $2}' | sort | uniq > /etc/chinadns-ng/chnlist.txt
 
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt -O /etc/chinadns-ng/gfwlist.tmp
-mv -f /etc/chinadns-ng/gfwlist.tmp /etc/chinadns-ng/gfwlist.txt
-#sed -n -i '1,100p' /etc/chinadns-ng/gfwlist.txt
 
-wget https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -O /etc/chinadns-ng/chinalist.tmp
-mv -f /etc/chinadns-ng/chinalist.tmp /etc/chinadns-ng/chinalist.txt
-#sed -n -i '1,100p' /etc/chinadns-ng/chinalist.txt
+# update chnroute-nft
+data="$(curl -4fsSkL https://ftp.apnic.net/stats/apnic/delegated-apnic-latest | grep CN | grep ipv4)"
+echo "add table inet global" > /etc/chinadns-ng/chnroute.nftset
+echo "add set inet global chnroute { type ipv4_addr; flags interval; }" >> /etc/chinadns-ng/chnroute.nftset
+awk -F'|' '{printf("add element inet global chnroute { %s/%d }\n", $4, 32-log($5)/log(2))}' <<<"$data" >> /etc/chinadns-ng/chnroute.nftset
+
+
+# update chnroute6-nft
+data="$(curl -4fsSkL https://ftp.apnic.net/stats/apnic/delegated-apnic-latest | grep CN | grep ipv6)"
+echo "add table inet global" > /etc/chinadns-ng/chnroute6.nftset
+echo "add set inet global chnroute6 { type ipv6_addr; flags interval; }" >> /etc/chinadns-ng/chnroute6.nftset
+awk -F'|' '{printf("add element inet global chnroute6 { %s/%d }\n", $4, $5)}' <<<"$data" >> /etc/chinadns-ng/chnroute6.nftset
+
+
+# update gfwlist
+url='https://raw.githubusercontent.com/pexcn/daily/gh-pages/gfwlist/gfwlist.txt'
+data="$(curl -4fsSkL "$url" | grep -v -e '^[[:space:]]*$' -e '^[[:space:]]*#')"
+get_data() {
+    echo "$data"
+    # echo "google.cn"
+    echo "googleapis.cn"
+}
+get_data | sort | uniq >gfwlist.txt
+
+
 
 /etc/init.d/chinadns-ng restart
